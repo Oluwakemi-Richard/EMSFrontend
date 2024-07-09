8c84e503531c6da1664a64119f59b72fd7f56b47
Oluwakemi Richard <kemierichard@gmail.com>
2024-07-09 05:40:08 +0100

    Initial commit

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,7 +4,7 @@ const Register = () => {
   return (
     <div>
       <h2>Register</h2>
-      <p>This is the Register component.</p>
+      <p>This is the Register component. just here</p>
     </div>
   );
 };
